e7b01e9ad6dcfdebf3c28cf914ceef5ea0fcadbf
Safin Mahmud <safinm007@gmail.com>
2024-09-26 02:29:24 +0600

    enabled a20 bus

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -69,6 +69,11 @@ load32:
     mov SS, AX
     mov ebp, 0x00200000                  ; set the base pointer to 2MB
     mov esp, ebp                         ; set the stack pointer to 2MB
+
+    in AL, 0x92                          ; read the value of the keyboard controller
+    or AL, 2
+    out 0x92, AL                         ; write the value back to the keyboard controller
+    
     jmp $
 
 times 510 - ($ - $$) db 0                 ; fill the rest of the sector with 0 for boot signature

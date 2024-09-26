e0f80cb8b94cee535c444e746483ce1c55dd1d56
Safin Mahmud <safinm007@gmail.com>
2024-09-27 02:09:39 +0600

    i686-elf-ld build kernel

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    //todo
+    let output = Command::new("i686-elf-ld")
+        .args(&["-T", "linker.ld", "-g", "-r", ".build/kernel.asm.o"])
+        .output()
+        .expect("Failed to execute linker");
+
+    if !output.status.success() {
+        panic!("Linker error: {}", String::from_utf8_lossy(&output.stderr));
+    }
 }

bc7a930d7fd2fa5c93e670c40281708e6a91ceca
Safin Mahmud <safinm007@gmail.com>
2024-09-28 03:16:32 +0600

    cleaned main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    let output = Command::new("i686-elf-ld")
-        .args(&["-T", "linker.ld", "-g", "-r", ".build/kernel.asm.o"])
-        .output()
-        .expect("Failed to execute linker");
-
-    if !output.status.success() {
-        panic!("Linker error: {}", String::from_utf8_lossy(&output.stderr));
-    }
+    //todo
 }

dc379034d2d1842ca8e6d73d012f65fa8d9f89c9
Safin Mahmud <safinm007@gmail.com>
2024-10-01 00:47:55 +0600

    added hard disk driver

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -28,7 +28,7 @@ next:
     mov EAX, CR0
     or EAX, 0x1                           ; set the first bit of CR0 to 1
     mov CR0, EAX
-    jmp CODE_SEG:100000                  ; jump to the next instruction in 32-bit mode
+    jmp CODE_SEG:load32                ; jump to the next instruction in 32-bit mode
 
 
 gdt_start:
@@ -58,6 +58,60 @@ gdt_end:
 gdt_descriptor:
     dw gdt_end - gdt_start - 1            ; size of gdt
     dd gdt_start                          ; base address of gdt
+
+[BITS 32]
+load32:
+    mov EAX, 1
+    mov ECX, 100
+    mov EDI, 0x0100000
+    call ata_lba_read
+    jmp CODE_SEG:0x100000
+
+ata_lba_read:
+    mov EBX, EAX
+    shr EAX, 24
+    or EAX, 0xE0
+    mov DX, 0x1F6
+    out DX, AL
+
+    mov EAX, ECX
+    mov DX, 0x1F2
+    out DX, AL
+
+    mov EAX, EBX
+    mov DX, 0x1F3
+    out DX, AL
+
+    mov DX, 0x1F4
+    mov EAX, EBX
+    shr EAX, 8
+    out DX, AL
+
+    mov DX, 0x1F5
+    mov EAX, EBX
+    shr EAX, 16
+    out DX, AL
+
+    mov DX, 0x1F7
+    mov AL, 0x20
+    out DX, AL
+
+.next_sector:
+    push ECX
+
+.try_again:
+    mov DX, 0x1F7
+    in AL, DX
+    test AL, 8
+    jz .try_again
+
+    mov ECX, 256
+    mov DX, 0x1F0
+    rep insw
+    pop ECX
+    loop .next_sector
+    ret
+
 
 times 510 - ($ - $$) db 0                 ; fill the rest of the sector with 0 for boot signature
 dw 0xAA55                                 ; boot signature (little endian byte order for x86)

2ce4cd983b7a70277354e487fecaf2f19e92aca6
Safin Mahmud <safinm007@gmail.com>
2024-10-06 00:56:48 +0600

    added multiboot2

---
+++ src/boot/header.asm
@@ -0,0 +1,13 @@
+
+section .multiboot_header
+
+header_start:
+    dd 0xe85250D6 ;multiboot2 magic number
+    dd 0
+    dd header_end - header_start
+    dd 0x100000000 - (0xe85250D6 + 0 + (header_end - header_start))
+
+    dw 0
+    dw 0
+    dd 8
+header_end:
